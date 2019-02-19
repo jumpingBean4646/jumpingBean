8b2c54586e9438fc0960c315a9e935fe3d8af953
jumpingBean4646 <47793401+jumpingBean4646@users.noreply.github.com>
2019-02-19 13:06:34 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package beginTaleTwo;
+
+import javax.swing.*;
+
+/**
+     * Created by student on 2/19/2019.
+     */
+    public class HelloWorld {
+
+        public static void main(String[] args){
+
+            JFrame frame = new JFrame("WIndow Tech");
+
+            frame.setSize(400,400);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+        }
+    }
